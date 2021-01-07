b547c0d1456194b876a3207b5f027a7bc26a56b7
ciochetta <luisfelipeciochetta@gmail.com>
2021-01-06 23:21:55 -0300

    day 11

--- index.js
+++ index.js
@@ -6,22 +6,40 @@ LuisDB.evalObject({
 });
 
 LuisDB.evalObject({
-	type: "create",
+	type: "create table",
 	params: {
 		table: "students",
 		columns: ["name", "grade"],
 	},
 });
 
-for (let index = 0; index < 100; index++) {
-	LuisDB.evalObject({
-		type: "insert",
-		params: {
-			table: "students",
-			document: [
-				Math.random().toString(36).substring(7),
-				Math.round(Math.random() * 10),
-			],
-		},
+commandObj = {
+	type: "BULK INSERT",
+	params: {
+		table: "students",
+		documents: [],
+	},
+};
+
+console.time("insert");
+
+for (let index = 0; index < 10000; index++) {
+	commandObj.params.documents.push({
+		name: Math.random().toString(36).substring(7),
+		grade: Math.round(Math.random() * 10),
 	});
 }
+
+LuisDB.evalObject(commandObj);
+
+console.timeEnd("insert");
+
+let selectAll = {
+	type: "select",
+	params: {
+		table: "students",
+		columns: "star",
+	},
+};
+
+//LuisDB.evalObject(selectAll);
